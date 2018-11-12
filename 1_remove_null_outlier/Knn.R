dbb3c9bf5968362479727cd5962cae4bae56f284
tsangjustin <justintcoding@gmail.com>
2018-11-11 22:32:16 -0500

    Fix features:

--- 1_remove_null_outlier/Knn.R
+++ 1_remove_null_outlier/Knn.R
@@ -37,17 +37,22 @@ df <- read.csv(
 #               "SUSPECTS_ACTIONS_IDENTIFY_CRIME_PATTERN_FLAG",
 #               "SUSPECT_REPORTED_AGE", "SUSPECT_SEX", "SUSPECT_RACE_DESCRIPTION", "SUSPECT_HEIGHT", "SUSPECT_WEIGHT",
 #               "STOP_LOCATION_PRECINCT")
-features <- c("SUSPECTED_CRIME_DESCRIPTION",
-              "FRISKED_FLAG", "SEARCHED_FLAG", "OTHER_CONTRABAND_FLAG", "FIREARM_FLAG", "KNIFE_CUTTER_FLAG",
-              "OTHER_WEAPON_FLAG", "WEAPON_FOUND_FLAG", "PHYSICAL_FORCE_HANDCUFF_SUSPECT_FLAG",
-              "BACKROUND_CIRCUMSTANCES_VIOLENT_CRIME_FLAG", "BACKROUND_CIRCUMSTANCES_SUSPECT_KNOWN_TO_CARRY_WEAPON_FLAG",
-              "SUSPECTS_ACTIONS_CONCEALED_POSSESSION_WEAPON_FLAG", "SUSPECTS_ACTIONS_DRUG_TRANSACTIONS_FLAG",
-              "SUSPECTS_ACTIONS_IDENTIFY_CRIME_PATTERN_FLAG",
-              "SUSPECT_REPORTED_AGE", "SUSPECT_SEX", "SUSPECT_RACE_DESCRIPTION",
-              "STOP_LOCATION_PRECINCT")
 # features <- c("SUSPECTED_CRIME_DESCRIPTION",
-#               "SEARCHED_FLAG", "OTHER_CONTRABAND_FLAG", "FIREARM_FLAG", "KNIFE_CUTTER_FLAG",
-#               "OTHER_WEAPON_FLAG", "WEAPON_FOUND_FLAG")
+#               "FRISKED_FLAG", "SEARCHED_FLAG", "OTHER_CONTRABAND_FLAG", "FIREARM_FLAG", "KNIFE_CUTTER_FLAG",
+#               "OTHER_WEAPON_FLAG", "WEAPON_FOUND_FLAG", "PHYSICAL_FORCE_HANDCUFF_SUSPECT_FLAG",
+#               "BACKROUND_CIRCUMSTANCES_VIOLENT_CRIME_FLAG", "BACKROUND_CIRCUMSTANCES_SUSPECT_KNOWN_TO_CARRY_WEAPON_FLAG",
+#               "SUSPECTS_ACTIONS_CONCEALED_POSSESSION_WEAPON_FLAG", "SUSPECTS_ACTIONS_DRUG_TRANSACTIONS_FLAG",
+#               "SUSPECTS_ACTIONS_IDENTIFY_CRIME_PATTERN_FLAG",
+#               "SUSPECT_REPORTED_AGE", "SUSPECT_SEX", "SUSPECT_RACE_DESCRIPTION",
+#               "STOP_LOCATION_PRECINCT")
+features <- c(
+  "SUSPECTED_CRIME_DESCRIPTION",
+  "SEARCHED_FLAG",
+  "WEAPON_FOUND_FLAG",
+  "FIREARM_FLAG",
+  "OTHER_CONTRABAND_FLAG",
+  "SEARCH_BASIS_INCIDENTAL_TO_ARREST_FLAG",
+  "STOP_LOCATION_PRECINCT")
 dependent <- c("SUSPECT_ARRESTED_FLAG")
 
 ranks <- c("POF", "POM", "DT1", "DT2", "DT3", "DTS", "SSA", "SGT", "SDS", "LSA", "LT", "CPT", "DI", "LCD")
@@ -83,12 +88,59 @@ mmnorm <- function(x,minx,maxx) {
 ##### Cast to correct data type #####
 for (feature in c(features, dependent)) {
   # Should be factor
-  if (feature == "FRISKED_FLAG" || feature == "SEARCHED_FLAG" || feature == "OTHER_CONTRABAND_FLAG" ||
-      feature == "FIREARM_FLAG" || feature == "KNIFE_CUTTER_FLAG" || feature == "OTHER_WEAPON_FLAG" || feature == "WEAPON_FOUND_FLAG" ||
-      feature == "PHYSICAL_FORCE_HANDCUFF_SUSPECT_FLAG" || feature == "BACKROUND_CIRCUMSTANCES_VIOLENT_CRIME_FLAG" ||
-      feature == "BACKROUND_CIRCUMSTANCES_SUSPECT_KNOWN_TO_CARRY_WEAPON_FLAG" || feature == "SUSPECTS_ACTIONS_CONCEALED_POSSESSION_WEAPON_FLAG" ||
-      feature == "SUSPECTS_ACTIONS_DRUG_TRANSACTIONS_FLAG" || feature == "SUSPECTS_ACTIONS_IDENTIFY_CRIME_PATTERN_FLAG" ||
-      feature == "SUSPECT_ARRESTED_FLAG") {
+  if (feature == "STOP_FRISK_TIME_MINUTES") {
+    min_feature <- min(sqf_df[, feature])
+    max_feature <- max(sqf_df[, feature])
+    sqf_df[, feature] <- mmnorm(sqf_df[, feature], min_feature, max_feature)
+  } else if (feature == "DAY2") {
+    sqf_df[, feature] <- factor(sqf_df[, feature], levels = days)
+  } else if (feature == "MONTH2") {
+    sqf_df[, feature] <- factor(sqf_df[, feature], levels = months)
+  } else if (feature == "ISSUING_OFFICER_RANK" ||
+     feature == "SUPERVISING_OFFICER_RANK") {
+    sqf_df[, feature] <- factor(sqf_df[, feature], ranks)
+  } else if (feature == "STOP_DURATION_MINUTES") {
+    min_feature <- min(sqf_df[, feature])
+    max_feature <- max(sqf_df[, feature])
+    sqf_df[, feature] <- mmnorm(sqf_df[, feature], min_feature, max_feature)
+  } else if (feature == "OFFICER_EXPLAINED_STOP_FLAG" ||
+     feature == "OTHER_PERSON_STOPPED_FLAG" ||
+     feature == "OFFICER_IN_UNIFORM_FLAG" ||
+     feature == "ID_CARD_IDENTIFIES_OFFICER_FLAG" ||
+     feature == "SHIELD_IDENTIFIES_OFFICER_FLAG" ||
+     feature == "VERBAL_IDENTIFIES_OFFICER_FLAG" ||
+     feature == "FRISKED_FLAG" ||
+     feature == "SEARCHED_FLAG" ||
+     feature == "OTHER_CONTRABAND_FLAG" ||
+     feature == "FIREARM_FLAG" ||
+     feature == "KNIFE_CUTTER_FLAG" ||
+     feature == "OTHER_WEAPON_FLAG" ||
+     feature == "WEAPON_FOUND_FLAG" ||
+     feature == "PHYSICAL_FORCE_CEW_FLAG" ||
+     feature == "PHYSICAL_FORCE_DRAW_POINT_FIREARM_FLAG" ||
+     feature == "PHYSICAL_FORCE_HANDCUFF_SUSPECT_FLAG" ||
+     feature == "PHYSICAL_FORCE_OC_SPRAY_USED_FLAG" ||
+     feature == "PHYSICAL_FORCE_OTHER_FLAG" ||
+     feature == "PHYSICAL_FORCE_RESTRAINT_USED_FLAG" ||
+     feature == "PHYSICAL_FORCE_VERBAL_INSTRUCTION_FLAG" ||
+     feature == "PHYSICAL_FORCE_WEAPON_IMPACT_FLAG" ||
+     feature == "BACKROUND_CIRCUMSTANCES_VIOLENT_CRIME_FLAG" ||
+     feature == "BACKROUND_CIRCUMSTANCES_SUSPECT_KNOWN_TO_CARRY_WEAPON_FLAG" ||
+     feature == "SUSPECTS_ACTIONS_CASING_FLAG" ||
+     feature == "SUSPECTS_ACTIONS_CONCEALED_POSSESSION_WEAPON_FLAG" ||
+     feature == "SUSPECTS_ACTIONS_DECRIPTION_FLAG" ||
+     feature == "SUSPECTS_ACTIONS_DRUG_TRANSACTIONS_FLAG" ||
+     feature == "SUSPECTS_ACTIONS_IDENTIFY_CRIME_PATTERN_FLAG" ||
+     feature == "SUSPECTS_ACTIONS_LOOKOUT_FLAG" ||
+     feature == "SUSPECTS_ACTIONS_OTHER_FLAG" ||
+     feature == "SUSPECTS_ACTIONS_PROXIMITY_TO_SCENE_FLAG" ||
+     feature == "SEARCH_BASIS_ADMISSION_FLAG" ||
+     feature == "SEARCH_BASIS_CONSENT_FLAG" ||
+     feature == "SEARCH_BASIS_HARD_OBJECT_FLAG" ||
+     feature == "SEARCH_BASIS_INCIDENTAL_TO_ARREST_FLAG" ||
+     feature == "SEARCH_BASIS_OTHER_FLAG" ||
+     feature == "SEARCH_BASIS_OUTLINE_FLAG" ||
+     feature == "SUSPECT_ARRESTED_FLAG") {
     sqf_df[, feature] <- factor(sqf_df[, feature], levels = c("Y", "N"))
   } else if (feature == "ISSUING_OFFICER_RANK" ||
      feature == "SUPERVISING_OFFICER_RANK") {
@@ -107,6 +159,15 @@ for (feature in c(features, dependent)) {
     sqf_df[, feature] <- mmnorm(sqf_df[, feature], min_feature, max_feature)
   }
 }
+
+##### Need to make dummy data #####
+m_form <- as.formula(paste(" ~ ", paste(c(features, dependent), collapse = " + ")))
+m <- model.matrix(
+  m_form,
+  data = sqf_df
+)
+m <- m[, -c(1)]
+
 
 ##### Split data ######
 df_rows <- nrow(sqf_df)
@@ -134,19 +195,3 @@ for (k in k_seq) {
   print(table_k)
   print(paste("Accuracy: ", accuracy_k))
 }
-
-##### CART #####
-# library(rpart)
-# library(rpart.plot) # Enhance tree plot
-# library(rattle) # Fancy tree plot
-# library(RColorBrewer) # Color needed for rattle
-# 
-# myTree <- rpart(
-#   SUSPECT_ARRESTED_FLAG ~ ., # Build model where Survived dependent on rest features
-#   data=sqf_df  
-# )
-# 
-# ##### Visualize tree
-# prp(myTree)
-# 
-# fancyRpartPlot(myTree)
